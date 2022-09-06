6abf7229bfbb3534e865f49724da92a2bcee3fe2
smkim0310 <smkim0310mail@gmail.com>
2022-09-06 21:34:02 +0900

    modify text output

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -14,7 +14,7 @@ public class WordCRUD implements ICRUD{
     }
     @Override
     public Object add() {
-        System.out.print("=> 난이도(1,2,3)와 단어 입력: ");
+        System.out.print("\n=> 난이도(1,2,3)와 단어 입력: ");
         int level= kb.nextInt();
         String word=kb.nextLine();
 
@@ -44,11 +44,11 @@ public class WordCRUD implements ICRUD{
     }
 
     public void listAll(){
-        System.out.println("-----------------------");
+        System.out.println("\n-----------------------");
         for(int i=0;i<list.size();i++){
              System.out.print((i+1)+" ");
              System.out.println(list.get(i).toString());
         }
-        System.out.println("-----------------------");
+        System.out.println("-----------------------\n");
     }
 }
