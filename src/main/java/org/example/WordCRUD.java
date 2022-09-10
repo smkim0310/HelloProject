bae6b772f22a40b2af487f35f0c48df48fc688ad
smkim0310 <smkim0310mail@gmail.com>
2022-09-10 22:17:50 +0900

    loadFile method modified

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -124,7 +124,7 @@ public class WordCRUD implements ICRUD{
                 count++;
             }
             bf.close();
-            System.out.println("==> "+count+"개 로딩 완료!");
+            System.out.println("==> "+count+"개 단어 로딩 완료!");
         }
         catch (IOException e){
             e.printStackTrace();
