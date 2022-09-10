7f35659ff46f73b7a6679e806131062d864c5e88
smkim0310 <smkim0310mail@gmail.com>
2022-09-10 22:24:00 +0900

    new menu added

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -40,6 +40,9 @@ public class WordManager {
             else if(menu==6){
                 wordCRUD.deleteItem();
             }
+            else if(menu==7){
+                wordCRUD.saveFile();
+            }
 
         }
     }
