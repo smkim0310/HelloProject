3d99b07ecfaf10d47074d6b23cf938caa31a4d0b
smkim0310 <smkim0310mail@gmail.com>
2022-09-10 22:13:47 +0900

    loadFile method added

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -1,11 +1,16 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class WordCRUD implements ICRUD{
 
     ArrayList<Word> list;
+    final String fname="dictionary.txt";
     Scanner kb=new Scanner(System.in);
 
     WordCRUD(){
@@ -99,5 +104,30 @@ public class WordCRUD implements ICRUD{
             System.out.println("단어가 삭제되었습니다.");
         }
         else System.out.println("취소되었습니다.");
+    }
+
+    public void loadFile(){
+        try {
+            BufferedReader bf=new BufferedReader(new FileReader(fname));
+            String line;
+
+            int count=0;
+            while(true) {
+                line = bf.readLine();
+                if(line==null) break;
+
+                String data[] = line.split("\\|");
+                int level=Integer.parseInt(data[0]);
+                String word=data[1];
+                String meaning=data[2];
+                list.add(new Word(0,level,word,meaning));
+                count++;
+            }
+            bf.close();
+            System.out.println("==> "+count+"개 로딩 완료!");
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
