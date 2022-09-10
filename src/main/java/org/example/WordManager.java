c58a71c879942ffa4d2eb90759d2b896c275ddef
smkim0310 <smkim0310mail@gmail.com>
2022-09-10 22:51:37 +0900

    new menu added

--- src/main/java/org/example/WordManager.java
+++ src/main/java/org/example/WordManager.java
@@ -31,6 +31,9 @@ public class WordManager {
             if(menu==1){
                 wordCRUD.listAll();
             }
+            else if(menu==2){
+                wordCRUD.searchLevel();
+            }
             else if(menu==4){
                 wordCRUD.addWord();
             }
