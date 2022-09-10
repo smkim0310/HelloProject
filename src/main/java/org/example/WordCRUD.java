cd356f24703a827e077bbc4ad97a85cd641b4879
smkim0310 <smkim0310mail@gmail.com>
2022-09-10 22:35:42 +0900

    set new FileWriter for fname (dictionary.txt)

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -130,7 +130,7 @@ public class WordCRUD implements ICRUD{
 
     public void saveFile() {
         try {
-            PrintWriter pr=new PrintWriter(new FileWriter("test.txt"));
+            PrintWriter pr=new PrintWriter(new FileWriter(fname));
             for(Word one:list){
                 pr.write(one.toFileString()+"\n");
             }
